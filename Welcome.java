db0f23a6e81c10931d527fc579ad1123105c015a
RahulVarma-007 <dvrv369@gmail.com>
2023-01-23 15:25:51 +0530

    emergency fixed

--- Welcome.java
+++ Welcome.java
@@ -5,7 +5,16 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
+		
+		System.out.println("After new branchx");
+		
+		System.out.println(error());
 
+	}
+	
+	
+	public static String error() {
+		return "error done";
 	}
 
 }
